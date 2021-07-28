3cbea8ece7d56ec57366868a0e5bbff48783fb9c
Luca Giozza <luca.giozza@gmail.com>
2021-07-28 17:43:19 +0200

    incluso database

---
+++ database.php
@@ -0,0 +1,72 @@
+<?php
+
+$dischi =[
+
+
+             [   
+              'title' => 'New Jersey',
+               'author' => 'Bon Jovi',
+               'year' => 1988,
+               'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+             ],
+             [   
+                'title' => 'Bad',
+                 'author' => 'Micahel Jackson',
+                 'year' => 1987,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Millenium',
+                 'author' => 'Backstreet Boys',
+                 'year' => 1999,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Come on Over',
+                 'author' => 'Shania Twain ',
+                 'year' => 1997,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Led Zeppelin IV ',
+                 'author' => 'Led Zeppelin',
+                 'year' => 1971,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Appetite for Destruction ',
+                 'author' => 'Guns N’Roses ',
+                 'year' => 1987,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Daydream',
+                 'author' => 'Mariah Carey ',
+                 'year' => 1995,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'The Dark Side of the Moon',
+                 'author' => 'Pink Floyd',
+                 'year' => 1973,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Back in Black ',
+                 'author' => 'AC/DC',
+                 'year' => 1980,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+               [   
+                'title' => 'Thriller',
+                 'author' => 'Michael Jackson',
+                 'year' => 1982,
+                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+               ],
+
+
+            ];
+    // chiususra arrai dischi
+
+
+?>
--- index-php.php
+++ index-php.php
@@ -2,78 +2,8 @@
 <!-- Prima Milestone: Stampiamo i dischi solo con l’utilizzo di PHP, 
 che stampa direttamente i dischi in pagina: al caricamento della
  pagina ci saranno tutti i dischi. -->
-
 <?php
-
-$dischi =[
-
-
-             [   
-              'title' => 'New Jersey',
-               'author' => 'Bon Jovi',
-               'year' => 1988,
-               'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-             ],
-             [   
-                'title' => 'Bad',
-                 'author' => 'Micahel Jackson',
-                 'year' => 1987,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Millenium',
-                 'author' => 'Backstreet Boys',
-                 'year' => 1999,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Come on Over',
-                 'author' => 'Shania Twain ',
-                 'year' => 1997,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Led Zeppelin IV ',
-                 'author' => 'Led Zeppelin',
-                 'year' => 1971,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Appetite for Destruction ',
-                 'author' => 'Guns N’Roses ',
-                 'year' => 1987,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Daydream',
-                 'author' => 'Mariah Carey ',
-                 'year' => 1995,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'The Dark Side of the Moon',
-                 'author' => 'Pink Floyd',
-                 'year' => 1973,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Back in Black ',
-                 'author' => 'AC/DC',
-                 'year' => 1980,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-               [   
-                'title' => 'Thriller',
-                 'author' => 'Michael Jackson',
-                 'year' => 1982,
-                 'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-               ],
-
-
-            ];
-    // chiususra arrai dischi
-
-
+include 'database.php';
 ?>
 
 <!DOCTYPE html>
