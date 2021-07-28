65c79c732c39ca3f6974c7a3c24ed982d3b3874a
Luca Giozza <luca.giozza@gmail.com>
2021-07-28 14:54:57 +0200

    header

---
+++ index-php.php
@@ -0,0 +1,34 @@
+
+<!-- Prima Milestone: Stampiamo i dischi solo con l’utilizzo di PHP, 
+che stampa direttamente i dischi in pagina: al caricamento della
+ pagina ci saranno tutti i dischi. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <img class="disco-header" src="img/header.jfif" alt="">
+        <h1 class="title">Libreria Dischi</h1>
+        <button class="header-btn">Accedi</button>
+        <button class="header-btn-sx">Registrati</button>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        
+    </footer>
+   
+    
+</body>
+</html>
